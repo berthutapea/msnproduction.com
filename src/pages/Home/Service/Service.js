1fea93a1021b2d26b630bc14f4c4c7effe572198
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-24 22:19:06 +0700

    Refactor

--- src/pages/Home/Service/Service.js
+++ src/pages/Home/Service/Service.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-import { BiPalette, BiLaptop, BiMobile } from "react-icons/bi";
-// import { FaDuotone } from "react-icons/fa";
+import Services from "../../../assets/Reviews";
+// import { BiPalette, BiLaptop, BiMobile } from "react-icons/bi";
+import { FaBullhorn, FaLaptop, FaMobileAlt } from "react-icons/fa";
+// import { CheckCircle } from "react-feather";
 // import { FiServer } from "react-icons/fi";
 import {
   headingAnimation,
@@ -27,23 +29,23 @@ const Service = () => {
     {
       id: 1,
       title: "Website & Mobile Applications",
-      icon: <BiMobile />,
+      icon: <FaMobileAlt />,
       description:
-        "Kembangkan sistem informasi, sistem administrasi, atau sistem promosi, berbasis website & aplikasi mobile sesuai kebutuhan untuk meningkatkan keuntungan dan efisiensi operasional bisnis Anda.",
+        "Kembangkan sistem informasi, sistem administrasi, atau sistem promosi, berbasis website & aplikasi mobile sesuai kebutuhan untuk meningkatkan keuntungan dan efisiensi operasional bisnis Anda. \n ✅ iOS App Development \n ✅ Android App Development \n ✅ UI/UX Web/Mobile App Design \n ✅ WordPress Web Development \n ✅ Custom Web App Development",
     },
     {
       id: 2,
       title: "Branding & Content Creative",
-      icon: < BiPalette />,
+      icon: <FaBullhorn />,
       description:
-        "Tingkatkan reputasi bisnis Anda untuk membangun persepsi yang baik dan mudah dikenali di mata masyarakat dengan layanan Branding & Content Creative yang didambakan pelangggan bisnis Anda.",
+        "Tingkatkan reputasi bisnis Anda untuk membangun persepsi yang baik dan mudah dikenali di mata masyarakat dengan layanan Branding & Content Creative yang didambakan pelangggan bisnis Anda. \n ✅ Logo Design \n ✅ Stationary Design \n ✅ Marketing Kit Design \n ✅ Photo & Videography \n ✅ Social Media Content Design ",
     },
     {
       id: 3,
       title: "Digital Marketing & Advertising",
-      icon: <BiLaptop />,
+      icon: <FaLaptop />,
       description:
-        "Dapatkan pelanggan baru dan penjualan lebih banyak dengan bantuan layanan pemasaran online atau Digital Marketing & Advertising sesuai anggaran pemasaran, target market, dan jangkauan wilayah usaha Anda.",
+        "Dapatkan pelanggan baru dan penjualan lebih banyak dengan bantuan layanan pemasaran online atau Digital Marketing & Advertising sesuai anggaran pemasaran, target market, dan jangkauan wilayah usaha Anda. \n ✅ Tiktok Ads \n ",
     },
   ];
   return (
@@ -85,7 +87,14 @@ const Service = () => {
             <h2 className="text-2xl font-semibold text-center mb-4">
               {service.title}
             </h2>
-            <p className="text-accent">{service.description}</p>
+            <p className="text-accent">
+              {service.description.split("\n").map((s, i) => (
+                <React.Fragment key={i + 1}>
+                  <span className="text-gray-500 text-justify inline-block align-bottom mb-4">{s}</span>
+                  <br />
+                </React.Fragment>
+              ))}
+            </p>
           </div>
         ))}
       </motion.div>
