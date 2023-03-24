85605bb94d5e72a2b8415f1054ab9b8e50d569bc
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-24 16:43:50 +0700

    Refactor

--- src/components/BottomLine.js
+++ src/components/BottomLine.js
@@ -7,7 +7,7 @@ const BottomLine = () => {
       <div className="flex items-center justify-center mt-4">
         <div className="side-line"></div>
         <div className="middle-line">
-          <div className="inner-circle"></div>
+          {/* <div className="inner-circle"></div> */}
         </div>
         <div className="side-line"></div>
       </div>
