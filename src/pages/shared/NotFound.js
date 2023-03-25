5b5f26af014e8064754a27dee20f7914ac50ce85
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-25 20:57:21 +0700

    Refactor

--- src/pages/shared/NotFound.js
+++ src/pages/shared/NotFound.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <div className="h-screen w-full flex flex-col md:flex-row items-center justify-center">
-      <h1 className="text-[8rem] font-bold text-primary animate-bounce">404</h1>
+      <h1 className="text-[8rem] font-bold text-primary">404</h1>
       <div className="w-24 h-1 md:w-1 md:h-24 bg-primary my-6 md:my-0 md:mx-8"></div>
       <div className="flex flex-col items-center">
         <h2 className="text-2xl text-center mb-4">
