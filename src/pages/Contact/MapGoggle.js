e9d93d60d0896e3292263638753353e4ec0d15e7
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-25 20:48:42 +0700

    Refactor

--- src/pages/Contact/MapGoggle.js
+++ src/pages/Contact/MapGoggle.js
@@ -1,21 +1,21 @@
-import {
-    MapContainer,
-    TileLayer,
-    ZoomControl
-} from 'google-maps-react'
+// import {
+//     MapContainer,
+//     TileLayer,
+//     ZoomControl
+// } from 'google-maps-react'
 
-import "leaflet/dist/leaflet.css";
+// import "leaflet/dist/leaflet.css";
 
-const position = [10.63960, -63.25584]
+// const position = [10.63960, -63.25584]
 
-const Map = () => (
-    <MapContainer center={position} zoom={12} zoomControl={false}>
-        <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <ZoomControl position="bottomright" />
-    </MapContainer>
-);
+// const Map = () => (
+//     <MapContainer center={position} zoom={12} zoomControl={false}>
+//         <TileLayer
+//             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//         />
+//         <ZoomControl position="bottomright" />
+//     </MapContainer>
+// );
 
-export default Map;
+// export default Map;
