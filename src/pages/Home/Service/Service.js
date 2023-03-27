3c251f5cb0423e4cb20ba533f86b96c758488d1f
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-27 09:42:40 +0700

    Refactor

--- src/pages/Home/Service/Service.js
+++ src/pages/Home/Service/Service.js
@@ -1,13 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-// import Services from "../../../assets/Reviews";
-// import { BiPalette, BiLaptop, BiMobile } from "react-icons/bi";
-// import { FaBullhorn, FaLaptop, FaMobileAlt } from "react-icons/fa";~~
 import Services from "../../../utils/Services";
 import { FiArrowRight } from "react-icons/fi";
-// import { CheckCircle } from "react-feather";
-// import { FiServer } from "react-icons/fi";
 import {
   headingAnimation,
   sectionBodyAnimation,
@@ -27,30 +22,6 @@ const Service = () => {
       setViewDiv(false);
     }
   }, [inView, animation]);
-
-  // const services = [
-  //   {
-  //     id: 1,
-  //     title: "Website & Mobile Applications",
-  //     icon: <FaMobileAlt />,
-  //     description:
-  //       "Kembangkan sistem informasi, sistem administrasi, atau sistem promosi, berbasis website & aplikasi mobile sesuai kebutuhan untuk meningkatkan keuntungan dan efisiensi operasional bisnis di perusahaan Anda. \n ✅ iOS App Development \n ✅ Android App Development \n ✅ UI/UX Web/Mobile App Design \n ✅ WordPress Web Development \n ✅ Custom Web App Development",
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "Branding & Content Creative",
-  //     icon: <FaBullhorn />,
-  //     description:
-  //       "Tingkatkan reputasi bisnis Anda untuk membangun persepsi yang baik dan mudah dikenali di mata masyarakat dengan layanan Branding & Content Creative yang didambakan pelangggan bisnis Anda. \n ✅ Logo Design \n ✅ Stationary Design \n ✅ Marketing Kit Design \n ✅ Photo & Videography \n ✅ Social Media Content Design ",
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "Digital Marketing & Advertising",
-  //     icon: <FaLaptop />,
-  //     description:
-  //       "Dapatkan pelanggan baru dan penjualan lebih banyak dengan bantuan layanan pemasaran online atau Digital Marketing & Advertising sesuai anggaran pemasaran, target market, dan jangkauan wilayah usaha Anda. \n ✅ Tiktok Ads \n ✅ Google Search Ads \n ✅ GDN & Youtube Ads \n ✅ Facebook & Instagram Ads \n ✅ Search Engine Optimization",
-  //   },
-  // ];
   return (
     <div className="parent py-20 bg-neutral">
       <motion.div
