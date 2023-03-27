7112d078e5f0233c948daea2cb50747fdb9efce6
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-28 03:36:54 +0700

    Refactor

--- src/pages/About/Tim/Tim.js
+++ src/pages/About/Tim/Tim.js
@@ -1,73 +1,70 @@
-import React from "react";
-import Lottie from "react-lottie";
-import readingBook from "../../../assets/reading-book.json";
+import React, { useEffect, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import visiMisi from "../../../utils/visiMisi"
+import {
+  headingAnimation,
+  sectionBodyAnimation,
+} from "../../../hooks/useAnimation"
+import BottomLine from "../../../components/BottomLine";
+import "../../../components/Blur.css";
 
 const Tim = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: readingBook,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
+  const [ref, inView] = useInView();
+  const [viewDiv, setViewDiv] = useState(false);
+  const animation = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      setViewDiv(true);
+    } else {
+      setViewDiv(false);
+    }
+  }, [inView, animation]);
   return (
-    <div className="pt-24">
-      <div className="mb-12">
-        <h1 className="text-4xl font-semibold drop-shadow-md text-center">
-          My <span className="text-primary">Qualification</span>
+    <div className="parent py-20">
+      <motion.div
+        className="mb-12"
+        initial="hidden"
+        animate={viewDiv && "visible"}
+        variants={headingAnimation}
+      >
+        <h3 className="text-accent text-center">Tujuan & Arah</h3>
+        <h1 className="text-4xl font-semibold text-center">
+          Visi <span className="text-primary">& Misi</span>
         </h1>
-      </div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-8">
-        <div className="">
-          <Lottie
-            options={defaultOptions}
-            height="70%"
-            width="90%"
-            className="mx-auto lg:mr-auto"
-          />
-        </div>
-        <div className="mx-auto lg:ml-auto">
-          {/* Diploma */}
-          <div className="p-6 bg-[#313131] bg-opacity-90 backdrop-blur-lg rounded-lg shadow-lg inline-block w-[300px] hover:shadow-primary duration-300 cursor-pointer">
-            <div className="text-right mb-4">
-              <h3 className="text-2xl font-semibold text-primary">
-                Diploma in CSE
-              </h3>
-              <p className="text-sm text-neutral font-semibold">2021 - 2025</p>
-            </div>
-            <p className="text-sm text-neutral text-justify">
-              I am currently studying in Informatics Engineering (4th Semester) at Sekolah Tinggi Teknologi Informatika Sony Sugema, Karawang, Jawa Barat, Indonesia
-            </p>
-          </div>
+        <BottomLine />
+      </motion.div>
 
-          {/* React Developer */}
-          <div className="my-6 md:my-4 md:ml-[200px] p-6 bg-[#313131] bg-opacity-90 backdrop-blur-lg rounded-lg shadow-lg w-[300px] hover:shadow-primary duration-300 cursor-pointer">
-            <div className="text-right mb-4">
-              <h3 className="text-2xl font-semibold text-primary">
-                React Developer
-              </h3>
-              <p className="text-sm text-neutral font-semibold">2023</p>
+      <motion.div
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"
+        ref={ref}
+        initial="hidden"
+        animate={viewDiv && "visible"}
+        variants={sectionBodyAnimation}
+      >
+        {visiMisi?.map((visiMisi) => (
+          <div
+            key={visiMisi.id}
+            className={`${visiMisi.id % 2 === 0
+              ? "bg-base-100 impactfull-card shadow-lg"
+              : "bg-base-100 impactfull-card shadow-lg"
+              } rounded-lg p-6 duration-300`}
+          >
+            <div className="text-center mb-4">
+              <span className="text-primary text-5xl inline-block">
+                {visiMisi.icon}
+              </span>
             </div>
-            <p className="text-sm text-neutral text-justify">
-              I have successfully completed the <b>React Web Developer Expert</b>{" "}
-              from Dicoding.
+            <h2 className="text-2xl font-semibold text-center mb-4">
+              {visiMisi.title}
+            </h2>
+            <p className="text-accent">
+              {visiMisi.description}
             </p>
           </div>
-
-          {/* SSC */}
-          <div className="p-6 bg-[#313131] bg-opacity-90 backdrop-blur-lg rounded-lg shadow-lg w-[300px] hover:shadow-primary duration-300 cursor-pointer">
-            <div className="text-right mb-4">
-              <h3 className="text-2xl font-semibold text-primary">SSC</h3>
-              <p className="text-sm text-neutral font-semibold">2018 - 2021</p>
-            </div>
-            <p className="text-sm text-neutral text-justify">
-              I have passed the Senior Secondary Certificate (SSC) with the
-              highest result <b>GPA - 88</b>
-            </p>
-          </div>
-        </div>
-      </div>
+        ))}
+      </motion.div>
     </div>
   );
 };
