700c4c636943edfa66fd7da2123792b6754b2969
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-28 04:15:22 +0700

    Refactor

--- src/utils/visiMisi.js
+++ src/utils/visiMisi.js
@@ -1,24 +1,27 @@
-import { FaBullhorn, FaLaptop, FaMobileAlt } from "react-icons/fa";
+// import { FaBullhorn, FaLaptop, FaMobileAlt } from "react-icons/fa";
+import MisiMSN from "../assets/images/VisiMisiImg/misi.png"
+import LayananMSN from "../assets/images/VisiMisiImg/layanan.png"
+import VisiMSN from "../assets/images/VisiMisiImg/visi.png"
 
 const visiMisi = [
     {
         id: 1,
         title: "Misi",
-        icon: <FaMobileAlt />,
+        icon: { MisiMSN },
         description:
             "Menjadi Perusahaan swasta yang handal dalam penyedia jasa dan perdagangan sesuai dengan KBLI yang dimiliki.",
     },
     {
         id: 2,
         title: "Layanan",
-        icon: <FaBullhorn />,
+        icon: { LayananMSN },
         description:
             "Dapatkan loyalitas konsumen, berikan mereka pengalaman yang berkesan dengan menciptakan hubungan yang saling menguntungkan.",
     },
     {
         id: 3,
         title: "Visi",
-        icon: <FaLaptop />,
+        icon: { VisiMSN },
         description:
             "Dalam melaksanakan tugas - tugasnya Mulia Sejati Nusantara Production senantiasa menjaga, profesionalisme, kejujuran, kebersamaan, ketertiban, dan kesehatan kerja sehingga dihasilkan suatu karya yang berkualitas.",
     },
