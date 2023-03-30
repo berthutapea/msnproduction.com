f7521a04b859bc2703b4d26f4e79b9ddb2a78628
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-03-30 11:56:58 +0700

    Refactor

--- src/utils/blogs.js
+++ src/utils/blogs.js
@@ -1,17 +1,17 @@
 const blogs = [
   {
     _id: 1,
-    title: "Introduction to MERN Stack Developer",
-    path: "introduction-to-mern-stack-developer",
-    date: "21 Oct 2022",
-    img: "https://res.cloudinary.com/du541igfh/image/upload/v1679520807/Blog%20Portfolio%20Gilbert/blog-01_mqx2sc.jpg",
+    title: "5 Cara Optimasi SEO Instagram, Tingkatkan Visibilitas Akun Anda",
+    path: "5-cara-optimasi-seo-instagram-tingkatkan-visibilitas-akun-anda",
+    date: "09 September 2022",
+    img: "https://res.cloudinary.com/dh6s3dche/image/upload/v1680151015/Blog%20MSN/01-5_Cara_Optimasi_SEO_Instagram_Tingkatkan_Visibilitas_Akun_Mu_e0acku.jpg",
     instagram_link:
-      "https://msnproduction.com/",
+      "https://www.instagram.com/msn.production/",
     linkedin_link:
-      "https://www.linkedin.com/company/msnproduction/mycompany/",
-    tags: ["mern", "mernstackdeveloper", "mongodb", "expressjs", "reactjs", "nodejs", "introductionmern"],
+      "https://www.linkedin.com/in/mulia-sejati-nusantara-production-a766b3258/",
+    tags: ["instagram", "seo", "optimasi", "teknologi",],
     description:
-      "MERN stack is a popular combination of technologies used to develop web applications. It comprises four open-source technologies: MongoDB, ExpressJS, ReactJS, and NodeJS. Together, these technologies provide a comprehensive platform for building high-performance web applications. \n MongoDB is a document-based NoSQL database that stores data in flexible, JSON-like documents. It provides a scalable and high-performing database that can handle large amounts of data with ease. \n ExpressJS is a flexible and robust web application framework for NodeJS. It provides a simple and easy-to-use API that makes it easy to build web applications. \n ReactJS is a front-end JavaScript library used for building user interfaces. It is fast, efficient, and provides a simple and intuitive way to create reusable UI components. \n NodeJS is a JavaScript runtime environment that enables developers to build scalable and high-performing server-side applications. It uses an event-driven, non-blocking I/O model that makes it ideal for building real-time web applications. \n Together, these four technologies provide a full-stack platform for building web applications. Developers can use MERN stack to build complex and scalable applications, from simple web pages to complex, data-intensive applications. \n MERN stack provides several advantages over other web development technologies. Here are some of the main advantages: \n 1. Full-stack platform: MERN stack provides a comprehensive platform for building web applications, from the front-end to the back-end. This eliminates the need to use multiple technologies, which can be time-consuming and complicated. \n 2. High-performance: MERN stack is designed to be high-performing and scalable, making it ideal for building complex and data-intensive applications. \n 3. Easy to learn: MERN stack is based on popular and widely-used technologies, which means that it is easy to learn and has a large community of developers. \n 4. Large community: MERN stack has a large community of developers, which means that there are plenty of resources and support available for developers. \n 5. Flexible: MERN stack is flexible and can be used to build a wide range of applications, from simple web pages to complex, data-intensive applications. \n Thank you so much for taking your time. I hope you have gained a piece of basic knowledge about Introduction to MERN Stack Developer and how it works.",
+      "Instagram merupakan salah satu platform media sosial terbesar di dunia. Dengan lebih dari satu miliar pengguna aktif setiap bulannya, Instagram menyediakan peluang besar bagi bisnis dan individu untuk mempromosikan merek dan menjangkau khalayak yang lebih luas. Namun, untuk memanfaatkan potensi Instagram sepenuhnya, Anda harus memastikan bahwa akun Anda dioptimalkan untuk SEO. Di bawah ini adalah lima cara untuk melakukan optimasi SEO pada Instagram Anda dan meningkatkan visibilitas akunmu",
   },
 ];
 
