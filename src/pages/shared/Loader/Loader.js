a2fc84374ea7b3da72b1e90ad0b97b0a7b23be6d
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-04-01 05:07:16 +0700

    Refactor

--- src/pages/shared/Loader/Loader.js
+++ src/pages/shared/Loader/Loader.js
@@ -5,15 +5,6 @@ const Loader = () => {
   return (
     <>
       <div id="loader-container">
-        {/* Start::Loader 01 */}
-        {/* <div id="loader">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div> */}
-
-        {/* Start::Loader 02 */}
         <div className="loading">
           <div className="loading_circle loading_blue"></div>
           <div className="loading_circle loading_coral"></div>
