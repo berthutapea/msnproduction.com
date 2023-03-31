1df76fa90dd1d9080e4822233f0b73db55e9a914
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-04-01 05:37:14 +0700

    Refactor

--- src/pages/Home/Informasi/Informasi.js
+++ src/pages/Home/Informasi/Informasi.js
@@ -1,70 +1,88 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Link } from "react-router-dom";
 import placeholderImage from "../../../assets/images/placeholder.jpg";
 import blogs from "../../../utils/blogs";
 import BottomLine from "../../../components/BottomLine/BottomLine";
 import { FaAngleRight } from "react-icons/fa";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import {
+    headingAnimation,
+    sectionBodyAnimation,
+} from "../../../hooks/useAnimation";
 
 const Informasi = () => {
     const [currentPage] = useState(1);
     const [blogsPerPage] = useState(3);
+    const [ref, inView] = useInView();
+    const [viewDiv, setViewDiv] = useState(false);
+    const animation = useAnimation();;
     const indexOfLastBlog = currentPage * blogsPerPage;
     const indexOfFirstBlog = indexOfLastBlog - blogsPerPage;
     const currentBlogs = blogs
-        .sort((a, b) => new Date(b.date) - new Date(a.date)) // Menyortir posting blog dari yang terbaru ke yang terlama
+        .sort((a, b) => new Date(b.date) - new Date(a.date))
         .slice(indexOfFirstBlog, indexOfLastBlog);
-    // const totalPages = Math.ceil(blogs.length / blogsPerPage);
 
-    // const handleNextPageClick = () => {
-    //     if (currentPage < totalPages) {
-    //         setCurrentPage(currentPage + 1);
-    //     }
-    // };
-
-    // const handlePreviousPageClick = () => {
-    //     if (currentPage > 1) {
-    //         setCurrentPage(currentPage - 1);
-    //     }
-    // };
+    useEffect(() => {
+        if (inView) {
+            setViewDiv(true);
+        } else {
+            setViewDiv(false);
+        }
+    }, [inView, animation]);
 
     return (
         <div className=" parent py-16">
-            <h1 className="text-3xl font-semibold text-center">
-                Informasi <span className="text-primary">Terbaru</span>
-            </h1>
-            <BottomLine />
-            <div className="mt-10 grid grid-cols-1 md:grid-cols-3 gap-8">
-                {currentBlogs.map((blog) => {
-                    const { _id, title, date, img, description, path } = blog;
-                    return (
-                        <div
-                            key={_id}
-                            className="flex flex-col w-full items-center justify-between gap-8 rounded-lg min-h-32"
-                        >
-                            <div className="w-full">
-                                <Link to={"/blog/" + path}>
-                                    <LazyLoadImage
-                                        placeholderSrc={placeholderImage}
-                                        src={img}
-                                        className="rounded-lg blog_image cursor-pointer"
-                                    />
-                                </Link>
+            <motion.div
+                initial="hidden"
+                animate={viewDiv && "visible"}
+                variants={headingAnimation}
+            >
+                <h1 className="text-3xl font-semibold text-center">
+                    Informasi <span className="text-primary">Terbaru</span>
+                </h1>
+                <BottomLine />
+            </motion.div>
+            <div className="mt-10">
+                <motion.div
+                    className=" grid grid-cols-1 md:grid-cols-3 gap-8"
+                    ref={ref}
+                    initial="hidden"
+                    animate={viewDiv && "visible"}
+                    variants={sectionBodyAnimation}
+                >
+                    {currentBlogs.map((blog) => {
+                        const { _id, title, date, img, description, path } = blog;
+                        return (
+                            <div
+                                key={_id}
+                                className="flex flex-col w-full items-center justify-between gap-8 rounded-lg min-h-32"
+                            >
+                                <div className="w-full">
+                                    <Link to={"/blog/" + path}>
+                                        <LazyLoadImage
+                                            placeholderSrc={placeholderImage}
+                                            src={img}
+                                            className="rounded-lg blog_image cursor-pointer"
+                                        />
+                                    </Link>
+                                </div>
+                                <div className="w-full">
+                                    <h2 className="text-[22px] font-medium text-accent cursor-pointer hover:text-primary">
+                                        <Link to={"/blog/" + path}>{title}</Link>
+                                    </h2>
+                                    <span className="text-gray-600 text-sm">
+                                        <p>{date}</p>
+                                    </span>
+                                    <p className="text-accent mt-1 mb-6">
+                                        {description?.slice(0, 80)} ...
+                                    </p>
+                                </div>
                             </div>
-                            <div className="w-full">
-                                <h2 className="text-[22px] font-medium text-accent cursor-pointer hover:text-primary">
-                                    <Link to={"/blog/" + path}>{title}</Link>
-                                </h2>
-                                <span className="text-gray-600 text-sm">
-                                    <p>{date}</p>
-                                </span>
-                                <p className="text-accent mt-1 mb-6">
-                                    {description?.slice(0, 80)} ...
-                                </p>
-                            </div>
-                        </div>
-                    );
-                })}
+                        );
+                    })}
+                </motion.div>
             </div>
             <Link
                 to="/blog"
