47888658a6ddbabfb74e49c66fa02be10c6b82f4
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-04-01 15:23:40 +0700

    Refactor

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,14 +1,9 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-// import BottomLine from "../../components/BottomLine/BottomLine";
 
 const Blog = () => {
   return (
     <div className="px-2 md:px-12 py-16 min-h-screen ">
-      {/* <h1 className="text-3xl font-semibold text-center">
-        Informasi <span className="text-primary">Terbaru</span>
-      </h1> */}
-      {/* <BottomLine /> */}
       <Outlet />
     </div>
   );
