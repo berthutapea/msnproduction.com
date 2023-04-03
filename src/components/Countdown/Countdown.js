b18f357f76f10f03df0150ad8d7bfbd6e403ed4f
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-04-03 16:19:51 +0700

    Refactor

--- src/components/Countdown/Countdown.js
+++ src/components/Countdown/Countdown.js
@@ -33,7 +33,7 @@ const CountdownTimer = () => {
     };
 
     // Set tanggal target
-    const targetDate = new Date('April 27, 2023 00:00:00');
+    const targetDate = new Date('April 09, 2023 00:00:00');
 
     return (
         <div className="container mx-auto mt-10">
