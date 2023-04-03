d1dd642b4b2ba425301bd684852a8bc35646f24c
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-04-03 16:11:23 +0700

    Refactor

--- src/pages/Tentang/Tentang.js
+++ src/pages/Tentang/Tentang.js
@@ -1,16 +1,16 @@
-// import React from "react";
-// import TentangKami from "./TentangKami/TentangKami";
-// import VisiMisi from "./VisiMisi/VisiMisi";
-// import Tim from "./Tim/Tim";
+import React from "react";
+import TentangKami from "./TentangKami/TentangKami";
+import VisiMisi from "./VisiMisi/VisiMisi";
+import Tim from "./Tim/Tim";
 
-// const Tentang = () => {
-//   return (
-//     <div className="pt-16">
-//       <TentangKami />
-//       <VisiMisi />
-//       <Tim />
-//     </div>
-//   );
-// };
+const Tentang = () => {
+  return (
+    <div className="pt-16">
+      <TentangKami />
+      <VisiMisi />
+      <Tim />
+    </div>
+  );
+};
 
-// export default Tentang;
+export default Tentang;
