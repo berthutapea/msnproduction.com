3d9d7d3ad9c57a9a81833ff1301c00d8a3b2ffc7
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-05-01 19:53:01 +0700

    Refactor

--- src/pages/Paket/Paket.js
+++ src/pages/Paket/Paket.js
@@ -63,7 +63,7 @@ const Paket = () => {
               Segera Booking Slot Promo Sebelum Kehabisan!
             </h3>
             <h1 className="text-4xl font-semibold text-center">
-              Harga Paket Promo <span className="text-primary">Spesial Idul Fitri !</span>
+              Harga Paket Promo <span className="text-primary">Awal Bulan Mei</span>
             </h1>
             <BottomLine />
             <CountdownTimer />
