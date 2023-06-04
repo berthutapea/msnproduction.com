b915295b296a4e8534c2d71c18299bf120e1b147
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-06-04 20:29:44 +0700

    Refactor Countdown

--- src/components/atoms/Countdown/index.js
+++ src/components/atoms/Countdown/index.js
@@ -33,7 +33,7 @@ const CountdownTimer = () => {
     };
 
     // Set tanggal target
-    const targetDate = new Date('Jun 04, 2023 00:00:00');
+    const targetDate = new Date('Jun 11, 2023 00:00:00');
 
     return (
         <div className="container mx-auto mt-10">
