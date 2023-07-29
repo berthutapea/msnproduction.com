95373a0df17d07a38a233754e157317b2f68dea2
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-07-29 21:24:18 +0700

    Refactor Countdown

--- src/components/atoms/Countdown/index.js
+++ src/components/atoms/Countdown/index.js
@@ -32,7 +32,7 @@ const CountdownTimer = () => {
         }
     };
 
-    const targetDate = new Date('Jul 30, 2023 00:00:00');
+    const targetDate = new Date('2023-08-06T00:00:00');
 
     return (
         <div className="container mx-auto mt-10">
