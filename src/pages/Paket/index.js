a6faeaaf18681ba9dbb34c5dd634ba0206bfd9c6
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-09-04 22:06:08 +0700

    refactor: countdown

--- src/pages/Paket/index.js
+++ src/pages/Paket/index.js
@@ -59,7 +59,7 @@ const Paket = () => {
               Segera Booking Slot Promo Sebelum Kehabisan!
             </h3>
             <h1 className="text-4xl font-semibold text-center">
-              Harga Paket Promo <span className="text-primary">Akhir Bulan Agustus</span>
+              Harga Paket Promo <span className="text-primary">Awal Bulan September</span>
             </h1>
             <BottomLine />
             <Countdown />
