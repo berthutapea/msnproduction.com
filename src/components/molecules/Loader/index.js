c76df35014207ebd008611009ae0d1b2c5385889
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-07-22 20:48:45 +0700

    fix: Loader

--- src/components/molecules/Loader/index.js
+++ src/components/molecules/Loader/index.js
@@ -1,18 +1,18 @@
-import React from "react";
-import "./Loader.css";
+// import React from "react";
+// import "./Loader.css";
 
-const Loader = () => {
-  return (
-    <>
-      <div id="loader-container">
-        <div className="loading">
-          <div className="loading_circle loading_blue"></div>
-          <div className="loading_circle loading_coral"></div>
-          <div className="loading_circle loading_orange"></div>
-        </div>
-      </div>
-    </>
-  );
-};
+// const Loader = () => {
+//   return (
+//     <>
+//       <div id="loader-container">
+//         <div className="loading">
+//           <div className="loading_circle loading_blue"></div>
+//           <div className="loading_circle loading_coral"></div>
+//           <div className="loading_circle loading_orange"></div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
 
-export default Loader;
+// export default Loader;
