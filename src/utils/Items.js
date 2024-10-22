fe9d5a0ac7aab5b2151318b633881c434955a42e
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-10-22 21:05:26 +0700

    fix: items

--- src/utils/Items.js
+++ src/utils/Items.js
@@ -1,145 +1,145 @@
-// import { FaCheck } from "react-icons/fa";
+import { FaCheck } from "react-icons/fa";
 
-// const Items = [
-//   {
-//     id: 1,
-//     title: "SIP",
-//     subtitle: "(Standar)",
-//     label: "Rp.",
-//     price: "139K",
-//     diskon: "240K",
-//     icon: <FaCheck />,
-//     description:
-//       "Desain Tema GOLD & PREMIUM \n Masa Aktif 6 bulan \n Revisi Max. 5 point \n Unlimited Share \n Hitungan Mundur \n Quotes / Kutipan Ayat \n Tombol Petunjuk Arah \n Custom Nama Tamu \n Bebas Request Musik \n Musik Auto-play \n Protokol Kesehatan Std. \n Tombol Menu \n Ucapan WhatsApp \n Foto di Galeri (Max. 5)",
-//     category: "wedding",
-//   },
-//   {
-//     id: 2,
-//     title: "BASIC",
-//     subtitle: "(Standar)",
-//     label: "Rp.",
-//     price: "199K",
-//     diskon: "329K",
-//     icon: <FaCheck />,
-//     description:
-//       "Desain Tema GOLD & PREMIUM \n Masa Aktif 12 bulan \n Revisi Max. 10 point \n Unlimited Share \n Hitungan Mundur \n Quotes / Kutipan Ayat \n Tombol Petunjuk Arah \n Custom Nama Tamu \n Bebas Request Musik \n Musik Auto-play \n Protokol Kesehatan Std. \n Tombol Menu \n Ucapan Wish Box \n Foto di Galeri (Max. 8) \n Video di Galeri/Love Story (Max. 1) \n Peta Google Maps \n Link Live Streaming \n Simpan ke Google Calender \n RSVP - Konfirmasi Kehadiran \n Amplop Digital (Max. 3 Rek)",
-//     category: "wedding",
-//   },
-//   {
-//     id: 3,
-//     title: "ADVANCE",
-//     subtitle: "(Full Fitur)",
-//     label: "Rp.",
-//     price: "249K",
-//     diskon: "499K",
-//     icon: <FaCheck />,
-//     description:
-//       "Desain Tema GOLD & PREMIUM \n Masa Aktif 12 bulan \n  Revisi UNLIMITED \n Unlimited Share \n Hitungan Mundur \n Quotes / Kutipan Ayat \n Tombol Petunjuk Arah \n Custom Nama Tamu \n Bebas Request Musik \n Musik Auto-play \n Protokol Kesehatan Std. \n Tombol Menu Ucapan Wish Box Foto di Galeri (Max. 12) \n Video di Galeri/Love Story (Max. 2) \n Peta Google Maps \n Link Live Streaming \n Simpan ke Google Calender \n RSVP - Konfirmasi Kehadiran \n Amplop Digital (Max. 3 Rek) \n Love Story (Max. 5 Timeline)",
-//     category: "wedding",
-//   },
-//   {
-//     id: 4,
-//     title: "PRO",
-//     subtitle: "(Full Fitur, Photo & Video Prewed)",
-//     price: "CALL US",
-//     diskon: "x.xxxK",
-//     icon: <FaCheck />,
-//     description:
-//       "Desain Tema GOLD & PREMIUM \n Masa Aktif 12 bulan \n Revisi UNLIMITED \n Unlimited Share \n Hitungan Mundur \n Quotes / Kutipan Ayat \n Tombol Petunjuk Arah \n Custom Nama Tamu \n Bebas Request Musik \n Musik Auto-play \n Protokol Kesehatan Std. \n Tombol Menu \n Ucapan Wish Box \n Foto di Galeri (Max. 12) \n Video di Galeri/Love Story (Max. 2) \n Peta Google Maps \n Link Live Streaming \n Simpan ke Google Calender \n RSVP - Konfirmasi Kehadiran \n Amplop Digital (Max. 3 Rek) \n Love Story (Max. 5 Timeline) \n Photogrpher PreWed \n Photogrpher Pelaminan \n Video Cinematic",
-//     category: "wedding",
-//   },
-//   {
-//     id: 5,
-//     title: "PAKET STARTER",
-//     subtitle: "(Standar)",
-//     label: "Rp.",
-//     price: "1.999K",
-//     diskon: "2.699K",
-//     icon: <FaCheck />,
-//     description:
-//       "Domain .com \n SSL Certificate \n Kapasitas 1GB (Shared Hosting) \n 1 Email Profesional \n 10+ Tema Pilihan \n Akses ke Dashboard Admin \n Fitur Manajemen Produk \n Fitur Manajemen Pemesanan (via keranjang / whatsapp) \n Fitur Manajemen Promosi \n Fitur Manajemen Pembayaran (Transfer bank Manual) \n Fitur Manajemen Pengiriman (JNE, POS, TIKI, J&T, Sicepat, dll) \n 2x Revisi Desain \n Dokumentasi & Video Tutorial \n Gratis Pelatihan Penggunaan Website \n Gratis Konsultasi Optimasi & Digital Marketing \n Garansi Seumur Hidup*",
-//     category: "tokoonline",
-//   },
-//   {
-//     id: 6,
-//     title: "PAKET UMKM",
-//     subtitle: "(Standar)",
-//     price: "CALL US",
-//     diskon: "x.xxxK",
-//     icon: <FaCheck />,
-//     description:
-//       "Domain .com \n SSL Certificate \n Kapasitas 10GB (Private Hosting) \n 10 Email Profesional \n Request Desain Website \n Akses ke Dashboard Admin \n Akses ke Cpanel \n Fitur Manajemen Produk \n Fitur Manajemen Pemesanan (via keranjang / whatsapp) \n Fitur Manajemen Promosi \n Fitur Manajemen Pembayaran (Transfer bank Manual) \n Fitur Manajemen Pengiriman (JNE, POS, TIKI, J&T, Sicepat, dll) \n 3x Revisi Desain \n Dokumentasi & Video Tutorial \n Gratis Pelatihan Penggunaan Website \n  Gratis Konsultasi Optimasi & Digital Marketing \n  Garansi Seumur Hidup*",
-//     category: "tokoonline",
-//   },
-//   {
-//     id: 7,
-//     title: "PAKET CORPORATE",
-//     subtitle: "(Full Fitur)",
-//     price: "CALL US",
-//     diskon: "x.xxxK",
-//     icon: <FaCheck />,
-//     description:
-//       "Domain .com / .id / .co.id \n SSL Certificate \n Kapasitas 40GB (Private Hosting) \n 30+ Email Profesional \n Request Desain Website \n 20+ Halaman Website \n Akses ke Dashboard Admin \n Akses ke Cpanel \n Fitur Manajemen Produk \n Fitur Manajemen Pemesanan (via keranjang / whatsapp) \n Fitur Manajemen Promosi \n Fitur Manajemen Pembayaran (Transfer bank Manual) \n Fitur Manajemen Pengiriman (JNE, POS, TIKI, J&T, Sicepat, dll) \n 5x Revisi Desain \n Dokumentasi & Video Tutorial \n Gratis Pelatihan Penggunaan Website \n Optimasi SEO on Page, Integrasi Google Webmaster, Google Analytics, & Facebook Pixel \n Gratis Konsultasi Optimasi & Digital Marketing \n Garansi Seumur Hidup*",
-//     category: "tokoonline",
-//   },
-//   {
-//     id: 8,
-//     title: "PAKET CORPORATE",
-//     subtitle: "(Full Fitur)",
-//     price: "CALL US",
-//     diskon: "x.xxxK",
-//     icon: <FaCheck />,
-//     description:
-//       "Domain .com / .id / .co.id \n SSL Certificate \n Kapasitas 40GB (Private Hosting) \n 30+ Email Profesional \n Request Desain Website \n 20+ Halaman Website \n Akses ke Dashboard Admin \n Akses ke Cpanel \n Fitur Manajemen Produk \n Fitur Manajemen Pemesanan (via keranjang / whatsapp) \n Fitur Manajemen Promosi \n Fitur Manajemen Pembayaran (Integrasi dengan payment gateway) \n Fitur Manajemen Pengiriman (JNE, POS, TIKI, J&T, Sicepat, dll) \n Fitur Faktur Order & Slip Pengiriman \n Integrasi Sosial Media (Facebook Catalogue, Instagram Shop) \n Otentikasi social media (Facebook & Gmail Login Register) \n Sales Popup \n Notifikasi Order (Email/SMS/Whatsapp) \n Progressive Web App (Aplikasi Mobile) \n 5x Revisi Desain \n Dokumentasi & Video Tutorial \n Gratis Pelatihan Penggunaan Website \n Optimasi SEO on Page, Integrasi Google Webmaster, Google Analytics, & Facebook Pixel \n Gratis Konsultasi Optimasi & Digital Marketing \n Garansi Seumur Hidup*",
-//     category: "tokoonline",
-//   },
-//   {
-//     id: 9,
-//     title: "PAKET STARTER",
-//     subtitle: "(Ideal Untuk Pemula)",
-//     label: "Rp.",
-//     price: "999K",
-//     diskon: "1.499K",
-//     icon: <FaCheck />,
-//     description:
-//       "Domain .com \n Kapasitas 1GB (Shared Hosting) \n 1 Email Profesional \n 10+ Tema Pilihan \n 8 Halaman Website \n Full Akses Admin \n 2x Revisi Desain \n Gratis Video Tutorial & Pelatihan \n Gratis Konsultasi Optimasi & Digital Marketing \n Garansi Seumur Hidup*",
-//     category: "companyprofile",
-//   },
-//   {
-//     id: 10,
-//     title: "PAKET UMKM",
-//     subtitle: "(Ideal Untuk Usaha Menengah)",
-//     price: "CALL US",
-//     diskon: "x.xxxK",
-//     icon: <FaCheck />,
-//     description:
-//       "Domain .com \n SSL Certificate \n Kapasitas 10GB (Private Hosting) \n 10 Email Profesional \n Request Desain Website \n 8-20 Halaman Website \n Full Akses Admin & cPanel \n 3x Revisi Desain \n Gratis Video Tutorial & Pelatihan \n Gratis Konsultasi Optimasi & Digital Marketing \n Garansi Seumur Hidup*",
-//     category: "companyprofile",
-//   },
-//   {
-//     id: 11,
-//     title: "PAKET CORPORATE",
-//     subtitle: "(Ideal Untuk Instansi & Perusahaan)",
-//     price: "CALL US",
-//     diskon: "x.xxxK",
-//     icon: <FaCheck />,
-//     description:
-//       "Domain .com / .id / .co.id \n SSL Certificate \n Kapasitas 40GB (Private Hosting) \n 30+ Email Profesional \n Request Desain Website \n 20+ Halaman Website \n Full Akses Admin & cPanel \n 5x Revisi Desain \n Gratis Video Tutorial & Pelatihan \n Optimasi SEO on Page, Integrasi Google Webmaster, Google Analytics, & Facebook Pixel \n Gratis Konsultasi Optimasi & Digital Marketing \n Garansi Seumur Hidup*",
-//     category: "companyprofile",
-//   },
-//   {
-//     id: 12,
-//     title: "PAKET CORPORATE",
-//     subtitle: "(Ideal Untuk Instansi & Perusahaan)",
-//     price: "CALL US",
-//     diskon: "x.xxxK",
-//     icon: <FaCheck />,
-//     description:
-//       "Domain .com / .id / .co.id \n SSL Certificate \n Kapasitas 40GB (Private Hosting) \n 30+ Email Profesional \n Request Desain Website \n 20+ Halaman Website \n Full Akses Admin & cPanel \n 5x Revisi Desain \n Otentikasi social media (Facebook & Gmail Login Register) \n Sales Popup \n Notifikasi (Email/SMS/Whatsapp) \n Progressive Web App (Aplikasi Mobile) \n Gratis Video Tutorial & Pelatihan \n ptimasi SEO on Page, Integrasi Google Webmaster, Google Analytics, & Facebook Pixel \n Gratis Konsultasi Optimasi & Digital Marketing \n Garansi Seumur Hidup*",
-//     category: "companyprofile",
-//     price: "CALL US",
-//     diskon: "x.xxxK",
-//   },
-// ];
+const Items = [
+  {
+    id: 1,
+    title: "SIP",
+    subtitle: "(Standar)",
+    label: "Rp.",
+    price: "139K",
+    diskon: "240K",
+    icon: <FaCheck />,
+    description:
+      "Desain Tema GOLD & PREMIUM \n Masa Aktif 6 bulan \n Revisi Max. 5 point \n Unlimited Share \n Hitungan Mundur \n Quotes / Kutipan Ayat \n Tombol Petunjuk Arah \n Custom Nama Tamu \n Bebas Request Musik \n Musik Auto-play \n Protokol Kesehatan Std. \n Tombol Menu \n Ucapan WhatsApp \n Foto di Galeri (Max. 5)",
+    category: "wedding",
+  },
+  {
+    id: 2,
+    title: "BASIC",
+    subtitle: "(Standar)",
+    label: "Rp.",
+    price: "199K",
+    diskon: "329K",
+    icon: <FaCheck />,
+    description:
+      "Desain Tema GOLD & PREMIUM \n Masa Aktif 12 bulan \n Revisi Max. 10 point \n Unlimited Share \n Hitungan Mundur \n Quotes / Kutipan Ayat \n Tombol Petunjuk Arah \n Custom Nama Tamu \n Bebas Request Musik \n Musik Auto-play \n Protokol Kesehatan Std. \n Tombol Menu \n Ucapan Wish Box \n Foto di Galeri (Max. 8) \n Video di Galeri/Love Story (Max. 1) \n Peta Google Maps \n Link Live Streaming \n Simpan ke Google Calender \n RSVP - Konfirmasi Kehadiran \n Amplop Digital (Max. 3 Rek)",
+    category: "wedding",
+  },
+  {
+    id: 3,
+    title: "ADVANCE",
+    subtitle: "(Full Fitur)",
+    label: "Rp.",
+    price: "249K",
+    diskon: "499K",
+    icon: <FaCheck />,
+    description:
+      "Desain Tema GOLD & PREMIUM \n Masa Aktif 12 bulan \n  Revisi UNLIMITED \n Unlimited Share \n Hitungan Mundur \n Quotes / Kutipan Ayat \n Tombol Petunjuk Arah \n Custom Nama Tamu \n Bebas Request Musik \n Musik Auto-play \n Protokol Kesehatan Std. \n Tombol Menu Ucapan Wish Box Foto di Galeri (Max. 12) \n Video di Galeri/Love Story (Max. 2) \n Peta Google Maps \n Link Live Streaming \n Simpan ke Google Calender \n RSVP - Konfirmasi Kehadiran \n Amplop Digital (Max. 3 Rek) \n Love Story (Max. 5 Timeline)",
+    category: "wedding",
+  },
+  {
+    id: 4,
+    title: "PRO",
+    subtitle: "(Full Fitur, Photo & Video Prewed)",
+    price: "CALL US",
+    diskon: "x.xxxK",
+    icon: <FaCheck />,
+    description:
+      "Desain Tema GOLD & PREMIUM \n Masa Aktif 12 bulan \n Revisi UNLIMITED \n Unlimited Share \n Hitungan Mundur \n Quotes / Kutipan Ayat \n Tombol Petunjuk Arah \n Custom Nama Tamu \n Bebas Request Musik \n Musik Auto-play \n Protokol Kesehatan Std. \n Tombol Menu \n Ucapan Wish Box \n Foto di Galeri (Max. 12) \n Video di Galeri/Love Story (Max. 2) \n Peta Google Maps \n Link Live Streaming \n Simpan ke Google Calender \n RSVP - Konfirmasi Kehadiran \n Amplop Digital (Max. 3 Rek) \n Love Story (Max. 5 Timeline) \n Photogrpher PreWed \n Photogrpher Pelaminan \n Video Cinematic",
+    category: "wedding",
+  },
+  {
+    id: 5,
+    title: "PAKET STARTER",
+    subtitle: "(Standar)",
+    label: "Rp.",
+    price: "1.999K",
+    diskon: "2.699K",
+    icon: <FaCheck />,
+    description:
+      "Domain .com \n SSL Certificate \n Kapasitas 1GB (Shared Hosting) \n 1 Email Profesional \n 10+ Tema Pilihan \n Akses ke Dashboard Admin \n Fitur Manajemen Produk \n Fitur Manajemen Pemesanan (via keranjang / whatsapp) \n Fitur Manajemen Promosi \n Fitur Manajemen Pembayaran (Transfer bank Manual) \n Fitur Manajemen Pengiriman (JNE, POS, TIKI, J&T, Sicepat, dll) \n 2x Revisi Desain \n Dokumentasi & Video Tutorial \n Gratis Pelatihan Penggunaan Website \n Gratis Konsultasi Optimasi & Digital Marketing \n Garansi Seumur Hidup*",
+    category: "tokoonline",
+  },
+  {
+    id: 6,
+    title: "PAKET UMKM",
+    subtitle: "(Standar)",
+    price: "CALL US",
+    diskon: "x.xxxK",
+    icon: <FaCheck />,
+    description:
+      "Domain .com \n SSL Certificate \n Kapasitas 10GB (Private Hosting) \n 10 Email Profesional \n Request Desain Website \n Akses ke Dashboard Admin \n Akses ke Cpanel \n Fitur Manajemen Produk \n Fitur Manajemen Pemesanan (via keranjang / whatsapp) \n Fitur Manajemen Promosi \n Fitur Manajemen Pembayaran (Transfer bank Manual) \n Fitur Manajemen Pengiriman (JNE, POS, TIKI, J&T, Sicepat, dll) \n 3x Revisi Desain \n Dokumentasi & Video Tutorial \n Gratis Pelatihan Penggunaan Website \n  Gratis Konsultasi Optimasi & Digital Marketing \n  Garansi Seumur Hidup*",
+    category: "tokoonline",
+  },
+  {
+    id: 7,
+    title: "PAKET CORPORATE",
+    subtitle: "(Full Fitur)",
+    price: "CALL US",
+    diskon: "x.xxxK",
+    icon: <FaCheck />,
+    description:
+      "Domain .com / .id / .co.id \n SSL Certificate \n Kapasitas 40GB (Private Hosting) \n 30+ Email Profesional \n Request Desain Website \n 20+ Halaman Website \n Akses ke Dashboard Admin \n Akses ke Cpanel \n Fitur Manajemen Produk \n Fitur Manajemen Pemesanan (via keranjang / whatsapp) \n Fitur Manajemen Promosi \n Fitur Manajemen Pembayaran (Transfer bank Manual) \n Fitur Manajemen Pengiriman (JNE, POS, TIKI, J&T, Sicepat, dll) \n 5x Revisi Desain \n Dokumentasi & Video Tutorial \n Gratis Pelatihan Penggunaan Website \n Optimasi SEO on Page, Integrasi Google Webmaster, Google Analytics, & Facebook Pixel \n Gratis Konsultasi Optimasi & Digital Marketing \n Garansi Seumur Hidup*",
+    category: "tokoonline",
+  },
+  {
+    id: 8,
+    title: "PAKET CORPORATE",
+    subtitle: "(Full Fitur)",
+    price: "CALL US",
+    diskon: "x.xxxK",
+    icon: <FaCheck />,
+    description:
+      "Domain .com / .id / .co.id \n SSL Certificate \n Kapasitas 40GB (Private Hosting) \n 30+ Email Profesional \n Request Desain Website \n 20+ Halaman Website \n Akses ke Dashboard Admin \n Akses ke Cpanel \n Fitur Manajemen Produk \n Fitur Manajemen Pemesanan (via keranjang / whatsapp) \n Fitur Manajemen Promosi \n Fitur Manajemen Pembayaran (Integrasi dengan payment gateway) \n Fitur Manajemen Pengiriman (JNE, POS, TIKI, J&T, Sicepat, dll) \n Fitur Faktur Order & Slip Pengiriman \n Integrasi Sosial Media (Facebook Catalogue, Instagram Shop) \n Otentikasi social media (Facebook & Gmail Login Register) \n Sales Popup \n Notifikasi Order (Email/SMS/Whatsapp) \n Progressive Web App (Aplikasi Mobile) \n 5x Revisi Desain \n Dokumentasi & Video Tutorial \n Gratis Pelatihan Penggunaan Website \n Optimasi SEO on Page, Integrasi Google Webmaster, Google Analytics, & Facebook Pixel \n Gratis Konsultasi Optimasi & Digital Marketing \n Garansi Seumur Hidup*",
+    category: "tokoonline",
+  },
+  {
+    id: 9,
+    title: "PAKET STARTER",
+    subtitle: "(Ideal Untuk Pemula)",
+    label: "Rp.",
+    price: "999K",
+    diskon: "1.499K",
+    icon: <FaCheck />,
+    description:
+      "Domain .com \n Kapasitas 1GB (Shared Hosting) \n 1 Email Profesional \n 10+ Tema Pilihan \n 8 Halaman Website \n Full Akses Admin \n 2x Revisi Desain \n Gratis Video Tutorial & Pelatihan \n Gratis Konsultasi Optimasi & Digital Marketing \n Garansi Seumur Hidup*",
+    category: "companyprofile",
+  },
+  {
+    id: 10,
+    title: "PAKET UMKM",
+    subtitle: "(Ideal Untuk Usaha Menengah)",
+    price: "CALL US",
+    diskon: "x.xxxK",
+    icon: <FaCheck />,
+    description:
+      "Domain .com \n SSL Certificate \n Kapasitas 10GB (Private Hosting) \n 10 Email Profesional \n Request Desain Website \n 8-20 Halaman Website \n Full Akses Admin & cPanel \n 3x Revisi Desain \n Gratis Video Tutorial & Pelatihan \n Gratis Konsultasi Optimasi & Digital Marketing \n Garansi Seumur Hidup*",
+    category: "companyprofile",
+  },
+  {
+    id: 11,
+    title: "PAKET CORPORATE",
+    subtitle: "(Ideal Untuk Instansi & Perusahaan)",
+    price: "CALL US",
+    diskon: "x.xxxK",
+    icon: <FaCheck />,
+    description:
+      "Domain .com / .id / .co.id \n SSL Certificate \n Kapasitas 40GB (Private Hosting) \n 30+ Email Profesional \n Request Desain Website \n 20+ Halaman Website \n Full Akses Admin & cPanel \n 5x Revisi Desain \n Gratis Video Tutorial & Pelatihan \n Optimasi SEO on Page, Integrasi Google Webmaster, Google Analytics, & Facebook Pixel \n Gratis Konsultasi Optimasi & Digital Marketing \n Garansi Seumur Hidup*",
+    category: "companyprofile",
+  },
+  {
+    id: 12,
+    title: "PAKET CORPORATE",
+    subtitle: "(Ideal Untuk Instansi & Perusahaan)",
+    price: "CALL US",
+    diskon: "x.xxxK",
+    icon: <FaCheck />,
+    description:
+      "Domain .com / .id / .co.id \n SSL Certificate \n Kapasitas 40GB (Private Hosting) \n 30+ Email Profesional \n Request Desain Website \n 20+ Halaman Website \n Full Akses Admin & cPanel \n 5x Revisi Desain \n Otentikasi social media (Facebook & Gmail Login Register) \n Sales Popup \n Notifikasi (Email/SMS/Whatsapp) \n Progressive Web App (Aplikasi Mobile) \n Gratis Video Tutorial & Pelatihan \n ptimasi SEO on Page, Integrasi Google Webmaster, Google Analytics, & Facebook Pixel \n Gratis Konsultasi Optimasi & Digital Marketing \n Garansi Seumur Hidup*",
+    category: "companyprofile",
+    price: "CALL US",
+    diskon: "x.xxxK",
+  },
+];
 
-// export default Items;
+export default Items;
