a13cb95ad6db48402959732bcc48abf39d101fc4
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-11-02 21:34:43 +0700

    fix: route

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import { Route, Routes, useLocation, Navigate } from "react-router-dom";
-import React, { Suspense } from "react";
-import { NotFound, Loader, ScrollToTop, Navbar, Footer, BlogDetail, BlogIndex } from "./components";
-import Blog from "./pages/Blog";
-import Kontak from "./pages/Kontak";
-import Tentang from "./pages/Tentang";
-const Home = React.lazy(() => import("./pages/Home"));
-const Paket = React.lazy(() => import("./pages/Paket"));
+// import { Route, Routes, useLocation, Navigate } from "react-router-dom";
+// import React, { Suspense } from "react";
+// import { NotFound, Loader, ScrollToTop, Navbar, Footer, BlogDetail, BlogIndex } from "./components";
+// import Blog from "./pages/Blog";
+// import Kontak from "./pages/Kontak";
+// import Tentang from "./pages/Tentang";
+// const Home = React.lazy(() => import("./pages/Home"));
+// const Paket = React.lazy(() => import("./pages/Paket"));
 
-function App() {
-  const location = useLocation();
-  const isFalse = location.pathname.includes("404");
-  return (
-    <>
-      <ScrollToTop />
-      {isFalse || <Navbar />}
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tentang" element={<Tentang />} />
-          <Route path="/paket" element={<Paket />} />
-          <Route path="/kontak" element={<Kontak />} />
-          <Route path="/blog" element={<Blog />}>
-            <Route index element={<BlogIndex />} />
-            <Route path=":blog_path" element={<BlogDetail />} />
-          </Route>
-          <Route path="/404" element={<NotFound />} />
-          <Route path="*" element={<Navigate replace to="/404" />}></Route>
-        </Routes>
-      </Suspense>
-      {isFalse || <Footer />}
-    </>
-  );
-}
+// function App() {
+//   const location = useLocation();
+//   const isFalse = location.pathname.includes("404");
+//   return (
+//     <>
+//       <ScrollToTop />
+//       {isFalse || <Navbar />}
+//       <Suspense fallback={<Loader />}>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/tentang" element={<Tentang />} />
+//           <Route path="/paket" element={<Paket />} />
+//           <Route path="/kontak" element={<Kontak />} />
+//           <Route path="/blog" element={<Blog />}>
+//             <Route index element={<BlogIndex />} />
+//             <Route path=":blog_path" element={<BlogDetail />} />
+//           </Route>
+//           <Route path="/404" element={<NotFound />} />
+//           <Route path="*" element={<Navigate replace to="/404" />}></Route>
+//         </Routes>
+//       </Suspense>
+//       {isFalse || <Footer />}
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
